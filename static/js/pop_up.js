856dfd9def049af2d174ea75ab950ada05fc48f4
servidor <servidor@DjangoGob.DjangoGob>
2018-05-09 14:04:32 -0300

    Se realizo el primer collectstatic

---
+++ static/js/pop_up.js
@@ -0,0 +1,18 @@
+$('.popup').on("click", function()
+{
+    $('.popuptext').removeClass("active");
+    $(this).find('.popuptext').addClass("active");
+    $('.popup-overlay').addClass("active");
+});
+
+$('.popuptext').on("click", function()
+{
+    $('.popuptext').removeClass("active");
+    $('.popup-overlay').removeClass("active");
+});
+
+$('.popup-overlay').on("click", function()
+{
+    $('.popup-overlay').removeClass("active");
+    $('.popuptext').removeClass("active");
+});
